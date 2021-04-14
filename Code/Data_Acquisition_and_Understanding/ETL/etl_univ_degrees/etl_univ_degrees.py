5be180e719cac4d7a0e5ff7f19cf14072e59f395
Fran <frmelchor@alumnos.unex.es>
2021-04-14 19:23:57 +0200

    Refactor etl_univ_degrees.py

--- Code/Data_Acquisition_and_Understanding/ETL/etl_univ_degrees/etl_univ_degrees.py
+++ Code/Data_Acquisition_and_Understanding/ETL/etl_univ_degrees/etl_univ_degrees.py
@@ -1,5 +1,5 @@
 import logging
-
+import swifter
 import numpy as np
 from apitep_utils import Date
 from apitep_utils import ETL
@@ -8,21 +8,7 @@ log = logging.getLogger(__name__)
 
 
 class UExETL(ETL):
-    COMPLETION_COURSE_ORIGINAL = "CursoFinalización"
-    STUDY_TYPE_ORIGINAL = "TipoEstudio"
-    TITLE_ORIGINAL = "Estudio"
-    IDENTITY_NUMBER_ORIGINAL = "DNI"
-    IDENTITY_NUMBER_ORIGINAL_PADDED = "DNI_PADDED"
-    GENDER_ORIGINAL = "Sexo"
-    BIRTH_DATE_ORIGINAL = "Fecha nacimiento"
     BIRTH_DATE_ORIGINAL_FORMATTED = "Fecha nacimiento_FORMATTED"
-    START_COURSE_ORIGINAL = "CursoInicio"
-    FAMILY_RESIDENCY_ORIGINAL = "Municipio/Provincia/País Residencia Familiar"
-    SECONDARY_SCHOOL_ORIGINAL = "Centro Secundaria"
-    PRE_REGISTRATION_ORDER_ORIGINAL = "OrdenPreinscripción"
-    ACCESS_TYPE_ORIGINAL = "TipoAcceso"
-    ACCESS_SUBTYPE_ORIGINAL = "SubTipoAcceso"
-    ACCESS_SUBTYPE_ORIGINAL_FIXED = "SubTipoAcceso_FIXED"
 
     IDENTIFIER_PROCESSED = "ID"
     COMPLETION_COURSE_PROCESSED = "CURSO_FIN"
@@ -48,7 +34,7 @@ class UExETL(ETL):
     number_of_studies_updated = 0
     columns_renamed = 0
 
-    @ETL.stopwatch
+    # @ETL.stopwatch
     def process(self):
         """
         Process UEx data.
@@ -59,28 +45,28 @@ class UExETL(ETL):
 
         # delete when birth date is empty
         rows_before = len(self.input_df.index)
-        self.input_df[UExETL.BIRTH_DATE_ORIGINAL].replace("", np.nan, inplace=True)
-        self.input_df.dropna(subset=[UExETL.BIRTH_DATE_ORIGINAL], inplace=True)
+        self.input_df[UExETL.BIRTH_DATE_PROCESSED].replace("", np.nan, inplace=True)
+        self.input_df.dropna(subset=[UExETL.BIRTH_DATE_PROCESSED], inplace=True)
         rows_after = len(self.input_df.index)
         self.no_birth_date_records_dropped = rows_before - rows_after
 
         # change birth date format (?)
         input_date_format = "%d/%m/%Y"
         self.input_df[UExETL.BIRTH_DATE_ORIGINAL_FORMATTED] = \
-            self.input_df[UExETL.BIRTH_DATE_ORIGINAL].swifter.progress_bar(False).apply(
+            self.input_df[UExETL.BIRTH_DATE_PROCESSED].swifter.progress_bar(False).apply(
                 Date.change_format,
                 args=(input_date_format,))
         self.birth_dates_formatted = self.replace_column(
             source_column=UExETL.BIRTH_DATE_ORIGINAL_FORMATTED,
-            destination_column=UExETL.BIRTH_DATE_ORIGINAL
+            destination_column=UExETL.BIRTH_DATE_PROCESSED
         )
 
         # delete duplicates
         rows_before = len(self.input_df.index)
         self.input_df.drop_duplicates(
             subset=[
-                UExETL.IDENTITY_NUMBER_ORIGINAL,
-                UExETL.TITLE_ORIGINAL],
+                UExETL.IDENTITY_NUMBER_PROCESSED,
+                UExETL.TITLE_PROCESSED],
             keep="first",
             inplace=True)
         rows_after = len(self.input_df.index)
@@ -118,14 +104,9 @@ class UExETL(ETL):
         log.info("Log dataset changes")
         log.debug("log_changes()")
 
-        log.info(f"- id cards formatted: {self.id_card_formatted}")
         log.info(f"- rows with no birth date dropped: {self.no_birth_date_records_dropped}")
         log.info(f"- birth dates formatted: {self.birth_dates_formatted}")
-        log.info(f"- access subtypes fixed: {self.access_subtype_fixed}")
-        log.info(f"- id cards encrypted: {self.id_cards_encrypted}")
         log.info(f"- duplicates deleted: {self.duplicates_deleted}")
-        log.info(f"- number of studies updated: {self.number_of_studies_updated}")
-        log.info(f"- columns renamed: {UExETL.COLUMNS_TO_RENAME}")
 
 
 def main():
